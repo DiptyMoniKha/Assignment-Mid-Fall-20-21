95c0233ecb1d69dfc7c01a701e8a9c1b2c222519
DiptyMoniKha <72722700+DiptyMoniKha@users.noreply.github.com>
2020-11-19 22:42:39 +0600

    Assignment Mid Fall 20-21
    Assignment Mid Fall 20-21

---
+++ Assignment-Mid-Fall-20-21/AssignmentMid Fall/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentMid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Department d = new Department("CSE");
+            Course oop2 = new Course("Course1", "Courseid");
+            Course oop1= new Course("Course1", "Courseid");
+            d.AddCourse(oop2);
+            d.AddCourse(oop1);
+            Section A = new Section("Section");
+            Section B = new Section("Section");
+            oop2.AddSection(A);
+            oop1.AddSection(B);
+            Faculty F = new Faculty("Faculty name", "Faculty ID",10.5f);
+            Faculty F2 = new Faculty("Dipty", "###ID###", 9.5f);
+            F.AddSection(A);
+            F2.AddSection(B);
+            A.AddTeacher(F);
+            B.AddTeacher(F2);
+            Console.WriteLine("\nCourse object");
+            oop1.ShowSectionFaculty();
+            Console.WriteLine("\nFaculty object");
+            F.ShowSectionDetails();
+            Console.ReadLine();
+        }
+   
+    }
+}
